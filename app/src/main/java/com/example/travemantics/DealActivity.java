e76f4ad5ab5a999aff43da0ad2863833a986111d
Kennedy Tirimba <ktirimba@yahoo.com>
2019-08-08 07:05:32 +0300

    Final version

---
+++ app/src/main/java/com/example/travemantics/DealActivity.java
@@ -0,0 +1,89 @@
+/* Kennedy Tirimba Moranga
+Android Phase 1 Challenge 2.0
+An android app that can read and write to a
+Firebase Realtime Database with both client and server sides
+* */
+
+
+package com.example.travemantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DealActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
+    TravelDeal deal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+
+        txtTitle = (EditText)findViewById(R.id.txtTitle);
+        txtPrice = (EditText)findViewById(R.id.txtPrice);
+        txtDescription = (EditText)findViewById(R.id.txtDescription);
+        Intent intent = getIntent();
+        TravelDeal deal= (TravelDeal) intent.getSerializableExtra("Deal");
+        if (deal== null){
+            deal = new TravelDeal();
+        }
+        this.deal= deal;
+        txtTitle.setText(deal.getTitle());
+        txtDescription.setText(deal.getDescription());
+        txtPrice.setText(deal.getPrice());
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()){
+            case R.id.save_menu:
+            saveDeal ();
+            Toast.makeText(this, "Deal Saved", Toast.LENGTH_LONG).show();
+            clean();
+            return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+    private void saveDeal(){
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        mDatabaseReference.push().setValue(deal);
+    }
+    private void clean (){
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
+
+    }
+}
